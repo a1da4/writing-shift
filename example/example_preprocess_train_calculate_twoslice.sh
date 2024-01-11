62d281abfacd96c765bc11b2c6b943b8d594a9da
a1da4 <ai.the.taichi@gmail.com>
2024-01-11 12:19:02 +0900

    fix example script

--- example/example_preprocess_train_calculate_twoslice.sh
+++ example/example_preprocess_train_calculate_twoslice.sh
@@ -1,15 +1,27 @@
 cd /work/src
 
-data_dir="/work/data"
+version=2
+data_dir="/work/data/v${version}"
+target_path="${data_dir}/target_ignore-zero.txt"
 t=1e-05
 thresh=20
-model_dir="/work/models/thresh-${thresh}"
-result_dir="/work/results/thresh-${thresh}"
+mkdir "/work/models/v${version}"
+mkdir "/work/results/v${version}"
+model_dir="/work/models/v${version}/thresh-${thresh}"
+result_dir="/work/results/v${version}/thresh-${thresh}"
 
-window=10
-dim=100
+window=5
+dim=50
 
-file_pathes="${data_dir}/magazine_1933-1949.txt ${data_dir}/magazine_1997-2013.txt"
+output_names="arawareru arawasu hakaru itadaku kaeru kaesu kiku koeru kosu naku tazuneru tukuru ukagau wakaru"
+analyze_target_path="${data_dir}/targets_for_distance/target-"
+analyze_target_pathes=""
+for output_name in ${output_names};
+do
+	analyze_target_pathes="${analyze_target_pathes} ${analyze_target_path}${output_name}.txt"
+done
+
+file_pathes="${data_dir}/magazine_1933-1957.txt ${data_dir}/magazine_1989-2013.txt"
 
 file_pathes_preprocessed=""
 
@@ -32,6 +44,11 @@ python3 obtain_sharedvocab.py \
 mkdir "${model_dir}"
 mv word2freq_* id2word* "${model_dir}"
 
+python3 add_targetwords_into_vocab.py \
+	--pickle_id2word "${model_dir}"/id2word.pkl \
+	--target_words "${target_path}"
+
+mv id2word.pkl "${model_dir}"
 
 # Training
 cd /work/src/sppmisvd
@@ -65,10 +82,34 @@ python3 joint_decompose.py \
 mv WV_d-"${dim}".npy "${model_dir}"/WV_w-"${window}"_d-"${dim}".npy
 
 
+# analyze
 python3 calculate_neighbors.py \
 	--pickle_id2word "${model_dir}"/id2word.pkl \
-	--joint_vector "${model_dir}"/WV_w-10_d-100.npy \
-	--target_words "${data_dir}"/target.txt 
+	--joint_vector "${model_dir}"/WV_w-"${window}"_d-"${dim}".npy \
+	--target_words "${target_path}" 
 
 mkdir "${result_dir}"
 mv result_targetword_neighbors.tsv "${result_dir}"/
+com
+
+python3 calculate_distance_targets.py \
+	--pickle_id2word "${model_dir}"/id2word.pkl \
+	--joint_vector "${model_dir}"/WV_w-"${window}"_d-"${dim}".npy \
+	--target_word_pathes ${analyze_target_pathes} \
+	--output_names ${output_names}
+
+for output_name in ${output_names};
+do
+	mv ${output_name}.tsv ${result_dir}
+done
+
+python3 visualize.py \
+	--pickle_id2word "${model_dir}"/id2word.pkl \
+	--joint_vector "${model_dir}"/WV_w-"${window}"_d-"${dim}".npy \
+	--target_word_pathes ${analyze_target_pathes} \
+	--output_names ${output_names} --num_neighbors 3
+
+for output_name in ${output_names};
+do
+	mv *${output_name}.png ${result_dir}
+done
