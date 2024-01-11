1b3e41bae08e96fd24139897dafc7ada1d82b6e3
a1da4 <ai.the.taichi@gmail.com>
2024-01-11 13:25:15 +0900

    rename script, fix calculate-distance

--- example/example_calculate_distance.sh
+++ example/example_calculate_distance.sh
@@ -1,18 +1,18 @@
 cd /work/src
 
-data_dir="/work/data/targets_for_distance"
+version=2
+data_dir="/work/data/v${version}"
 output_names="arawareru arawasu hakaru itadaku kaeru kaesu kiku koeru kosu naku tazuneru tukuru ukagau wakaru"
-target_path="${data_dir}/target-"
+target_path="${data_dir}/targets_for_distance/target-"
 target_pathes=""
 for output_name in ${output_names};
 do
 	target_pathes="${target_pathes} ${target_path}${output_name}.txt"
 done
-
 t=1e-05
 thresh=20
-model_dir="/work/models/thresh-${thresh}"
-result_dir="/work/results/thresh-${thresh}"
+model_dir="/work/models/v${version}/thresh-${thresh}_ignore-zero"
+result_dir="/work/results/v${version}/thresh-${thresh}_ignore-zero"
 
 window=5
 dim=50
@@ -22,6 +22,11 @@ python3 calculate_distance_targets.py \
 	--joint_vector "${model_dir}"/WV_w-"${window}"_d-"${dim}".npy \
 	--target_word_pathes ${target_pathes} \
 	--output_names ${output_names}
+
+python3 calculate_neighbors.py \
+	--pickle_id2word "${model_dir}"/id2word.pkl \
+	--joint_vector "${model_dir}"/WV_w-"${window}"_d-"${dim}".npy \
+	--target_words /work/data/v2/target_ignore-zero.txt
 
 mkdir "${result_dir}"
 mv *.tsv "${result_dir}"
